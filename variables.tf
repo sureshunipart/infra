77943c528c655a36f9e84ae61b5f4cbdca0f3884
ESPIRE\suresh.sharma <sureshsharma19822525@gmail.com>
2023-02-07 22:13:15 +0530

    updated

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable dockerhub_credentials{
+    type = string
+}
+
+variable codestar_connector_credentials {
+    type = string
+}
+
+variable "environment" {
+  description = "AWS enviornment"
+  type        = string
+  default     = "unipart-test"
+}
+
+variable "client" {
+  description = "AWS enviornment"
+  type        = string
+  default     = "Client-NHS"
+}
+
+variable "cost-center" {
+  description = "AWS enviornment"
+  type        = string
+  default     = "120-121313"
+}
+
+variable "directoryid" {
+  description = "directory_id"
+  type        = string
+  default     = "d-9367124f6d"
+}
+
+variable "bundleid" {
+  description = "directory_id"
+  type        = string
+  default     = "wsb-xc1mxf17p"
+}
